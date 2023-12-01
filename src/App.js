c5e6df95fe31f27af30ac85bddb4beebc8b395e8
Anastasiia Batarei <batarey.work@gmail.com>
2023-12-01 20:19:58 +0100

    Create SVGSprite and Icon components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { Icon, SVGSource } from './components/SVGSprite/SVGSprite';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Icon id="calendar" />
+      <Icon id="check" />
+      <Icon id="chevron" />
+      <Icon id="close" />
+      <Icon id="eyeclosed" />
+      <Icon id="eyeopened" />
+      <Icon id="logout" />
+      <Icon id="minus" />
+      <Icon id="plus" />
+      <Icon id="question" />
+      <Icon id="thumbsdown" />
+      <Icon id="thumbsup" />
+      <Icon id="upload" />
+      <Icon id="user" />
+
+      <SVGSource />
     </div>
   );
 }
---
+++ src/stories/SVGSprite.stories.js
@@ -0,0 +1,89 @@
+import { Icon, SVGSource } from '../components/SVGSprite/SVGSprite';
+
+export default {
+  title: 'Example/Icon',
+  component: Icon,
+  parameters: {},
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  argTypes: {},
+};
+
+export const Calendar = {
+  args: {
+    id: 'calendar',
+  },
+};
+
+export const Check = {
+  args: {
+    id: 'check',
+  },
+};
+
+export const Close = {
+  args: {
+    id: 'close',
+  },
+};
+
+export const EyeClosed = {
+  args: {
+    id: 'eyeclosed',
+  },
+};
+
+export const EyeOpened = {
+  args: {
+    id: 'eyeopened',
+  },
+};
+
+export const Logout = {
+  args: {
+    id: 'logout',
+  },
+};
+
+export const Minus = {
+  args: {
+    id: 'minus',
+  },
+};
+
+export const Plus = {
+  args: {
+    id: 'plus',
+  },
+};
+
+export const Question = {
+  args: {
+    id: 'question',
+  },
+};
+
+export const ThumbsDown = {
+  args: {
+    id: 'thumbsdown',
+  },
+};
+
+export const ThumbsUp = {
+  args: {
+    id: 'thumbssup',
+  },
+};
+
+export const Upload = {
+  args: {
+    id: 'upload',
+  },
+};
+
+export const User = {
+  args: {
+    id: 'user',
+  },
+};
