161e8b47a89cefc60e5b6f1f225bf30cd1e729ed
Anastasiia Batarei <batarey.work@gmail.com>
2023-12-08 19:03:32 +0100

    Prevent accidental double-clicking on the menu button

--- src/components/HamburgerMenu/HamburgerMenu.jsx
+++ src/components/HamburgerMenu/HamburgerMenu.jsx
@@ -1,32 +1,46 @@
 import './HamburgerMenu.css';
 
 import React from 'react';
+import debounce from 'lodash.debounce';
 
 const HamburgerMenu = () => {
   const [isOpened, setIsOpened] = React.useState(false);
-  const buttonLabel = isOpened ? 'Close menu' : 'Open menu';
+  const [isToggling, setIsToggling] = React.useState(false);
+
   const classes = isOpened
     ? 'hamburger-menu hamburger-menu--opened'
     : 'hamburger-menu';
+  const buttonLabel = isOpened ? 'Close menu' : 'Open menu';
   const buttonClasses = isOpened
     ? 'hamburger-menu__button hamburger-menu__button--close'
     : 'hamburger-menu__button';
+
+  const handleClick = () => {
+    if (isToggling) return;
+
+    setIsToggling(true);
+    setIsOpened(!isOpened);
+
+    setTimeout(() => {
+      setIsToggling(false);
+    }, 600); // 600ms is the duration of the animation
+  };
+
   return (
     <div className={classes}>
       <button
         className={buttonClasses}
         type="button"
         aria-label={buttonLabel}
-        onClick={() => {
-          setIsOpened(!isOpened);
-        }}
+        onClick={handleClick}
       >
         <span className="hamburger-menu__button-line"></span>
       </button>
       {/* FIXME inert={!isOpened} 
+          WARNING: Received `false` for a non-boolean attribute `inert`
           https://github.com/facebook/react/pull/24730
       */}
-      <div className="hamburger-menu__panel" inert={!isOpened && ''}>
+      <div className="hamburger-menu__panel" inert={!isOpened && 'inert'}>
         <nav className="hamburger-menu__navigation">
           <a className="hamburger-menu__link" href="#">
             Dashboard
