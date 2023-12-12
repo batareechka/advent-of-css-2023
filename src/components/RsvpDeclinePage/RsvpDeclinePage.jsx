62bc8f198dfa4fb377451e45235b8114acdf3456
Anastasiia Batarei <batarey.work@gmail.com>
2023-12-12 19:10:28 +0100

    Add RsvpDeclinePage

---
+++ src/components/RsvpDeclinePage/RsvpDeclinePage.jsx
@@ -0,0 +1,14 @@
+import WelcomeLayout from '../WelcomeLayout/WelcomeLayout';
+
+const RsvpDeclinePage = () => {
+  return (
+    <WelcomeLayout>
+      <h1>
+        <span className="title-1 text-decorative">Thanks</span>
+        <span className="title-jumbo">YOU’LL BE MISSED</span>
+      </h1>
+    </WelcomeLayout>
+  );
+};
+
+export default RsvpDeclinePage;
---
+++ src/components/RsvpDeclinePage/RsvpDeclinePage.stories.js
@@ -0,0 +1,11 @@
+import RsvpDeclinePage from './RsvpDeclinePage';
+
+export default {
+  title: 'Pages/RsvpDeclinePage',
+  component: RsvpDeclinePage,
+  parameters: {},
+  tags: ['autodocs'],
+  argTypes: {},
+};
+
+export const Default = {};
